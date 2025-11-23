96729252754943a9e91eed4f7aa48aace1538357
BolokhovVA <BolokhovVA@bcs.ru>
2025-11-23 11:19:04 +0300

    Detekt | Статический анализ кода в Kotlin [#505332]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
 }
 
 group = "ru.job4j"
@@ -15,10 +16,24 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    tasks.withType<io.gitlab.arturbosch.detekt.Detekt> {
+        reports {
+            html.required.set(true)
+            xml.required.set(true)
+            txt.required.set(false)
+        }
+    }
 }
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.1")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.1")
     testImplementation("org.assertj:assertj-core:3.24.2")
+}
+
+detekt {
+    toolVersion = "1.23.8"
+    config = files("$projectDir/detekt.yml")
+    buildUponDefaultConfig = true
+    allRules = false
 }
--- src/main/kotlin/ru/job4j/base/Defragmenter.kt
+++ src/main/kotlin/ru/job4j/base/Defragmenter.kt
@@ -3,13 +3,17 @@ package ru.job4j.base
 fun defragment(array: Array<String?>) {
     for ((i, element) in array.withIndex()) {
         if (element == null) {
-            for (j: Int in i + 1 until array.size) {
-                if (array[j] != null) {
-                    array[i] = array[j]
-                    array[j] = null
-                    break
-                }
-            }
+            moveNull(array, i)
+        }
+    }
+}
+
+private fun moveNull(array: Array<String?>, i: Int) {
+    for (j: Int in i + 1 until array.size) {
+        if (array[j] != null) {
+            array[i] = array[j]
+            array[j] = null
+            break
         }
     }
 }
