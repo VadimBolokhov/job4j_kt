3cf17e51139bbcba0c795d5b927660f69d8f8653
BolokhovVA <BolokhovVA@bcs.ru>
2025-12-14 12:46:47 +0300

    5. Класс-компаньон. [#191433]

--- src/main/kotlin/ru/job4j/oop/StartUI.kt
+++ src/main/kotlin/ru/job4j/oop/StartUI.kt
@@ -1,63 +1,66 @@
 package ru.job4j.oop
 
-object StartUI {
+class StartUI {
 
-    private const val MENU_ADD = "0"
-    private const val MENU_SHOW_ALL = "1"
-    private const val MENU_EXIT = "2"
+    companion object {
 
-    private val tracker = Tracker()
+        private const val MENU_ADD = "0"
+        private const val MENU_SHOW_ALL = "1"
+        private const val MENU_EXIT = "2"
 
-    fun init() {
-        var exit = false;
-        while (!exit) {
-            showMenu()
-            println("Выберите пункт меню: ")
-            val menuItem = readln()
-            when (menuItem) {
-                MENU_ADD -> createItem()
-                MENU_SHOW_ALL -> showAllItems()
-                MENU_EXIT -> exit = true
+        private val tracker = Tracker()
+
+        fun init() {
+            var exit = false;
+            while (!exit) {
+                showMenu()
+                println("Выберите пункт меню: ")
+                val menuItem = readln()
+                when (menuItem) {
+                    MENU_ADD -> createItem()
+                    MENU_SHOW_ALL -> showAllItems()
+                    MENU_EXIT -> exit = true
+                }
             }
         }
-    }
 
-    private fun showMenu() {
-        val menu = """
+        private fun showMenu() {
+            val menu = """
             Меню
             0. Добавить заявку
             1. Показать все заявки
             2. Выйти
         """.trimIndent()
-        println(menu)
-    }
-
-    private fun createItem() {
-        println("Добавление новой заявки")
-        println("Введите название заявки : ")
-        val name = readln()
-        println("Введите описание заявки : ")
-        val description = readln()
-        val newItem = Item(name = name, desc = description, created = System.currentTimeMillis())
-        tracker.add(newItem)
-        println("Создана заявка ID = ${newItem.id}")
-    }
-
-    private fun showAllItems() {
-        val items = tracker.getAll()
-        if (items.isEmpty()) {
-            println("Заявок нет")
-            return
+            println(menu)
         }
-        println("Список заявок:")
-        for ((i, item) in items.withIndex()) {
-            println("""
+
+        private fun createItem() {
+            println("Добавление новой заявки")
+            println("Введите название заявки : ")
+            val name = readln()
+            println("Введите описание заявки : ")
+            val description = readln()
+            val newItem = Item(name = name, desc = description, created = System.currentTimeMillis())
+            tracker.add(newItem)
+            println("Создана заявка ID = ${newItem.id}")
+        }
+
+        private fun showAllItems() {
+            val items = tracker.getAll()
+            if (items.isEmpty()) {
+                println("Заявок нет")
+                return
+            }
+            println("Список заявок:")
+            for ((i, item) in items.withIndex()) {
+                println("""
                 $i. ID=${item.id}
                 name=${item.name}
                 desc=${item.desc}
                 created=${item.created}
                 
                 """.trimIndent())
+            }
         }
     }
 }
